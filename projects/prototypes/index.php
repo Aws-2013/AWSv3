6d6ae15130fc7bec33533118a59176587fe4d62d
Francis David <francisd11385@yahoo.com>
2014-05-19 23:47:54 -0400

    Update file structure
    Update file structure

---
+++ projects/prototypes/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Arctic Web Solutions - For the Web and Beyond</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/ico" href="images/favicon.ico">
+	<?php //html5shiv enables HTML5 tags in older IE browsers ?>
+	<!--[if lt IE 9]>
+		<script src="dist/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!--[if lt IE 8]>
+	<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+	
+	<!-- Google Analytics -- use footer include -->
+	<script></script>
+</body>
+</html>
