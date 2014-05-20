0a3104e3a21926ebaf35af0d4b29a908a31a1d59
Francis David <francisd11385@yahoo.com>
2014-05-19 23:15:41 -0400

    ArcticWebSolutions ver 3.0
    ArcticWebSolutions will feature mobile first design, semantic HTML5 and
    CSS3, blog and much more

---
+++ index.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Arctic Web Solutions - For the Web and Beyond</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/ico" href="images/favicon.ico">
+	<?php //html5shiv enables HTML5 tags in older IE browsers ?>
+	<!--[if lt IE 9]>
+		<script src="dist/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!--[if lt IE 8]>
+	<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	
+	<div id="page">
+		<header class="site-header">
+			<div class="site-branding">
+				<h1 class="site-title">AWS-Arctic Web Solutions</h1>
+				<img src="images/aws-logo.png" alt="AWS - Arctic Web Solutions"/>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Expertise</a></li>
+					<li><a href="#">Portfolio</a></li>
+					<li><a href="#">Insipiration</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div class="page-contet">
+			<section class="about">
+				<header>
+					<h2>About</h2>
+				</header>
+				<p>I am a UX/UI design professional who has worked with the Web since 2001. I've been designing large scale web-based financial applications for over 7 years while working for a New York based financial technology vendor. This experience has allowed me to work on projects in various capacities including content analysis, workflow analysis, information architecture, information design, interaction design and implementation. Look around, and <mark><a class="contact">contact me</a></mark> if you are interested in discussing your project further.</p>
+				<dl>
+					<dt>View resume as:</dt>
+					<dd>Pdf</dd>
+					<dd>Doc</dd>
+					<dd>Html</dd>
+				</dl>
+				<aside>
+					<img src="self-portrait.png" alt="Picture of Me">
+					<a href="#">Connect on LinkedIN</a>
+				</aside>
+			</section>
+			<section class="expertise">
+				<header>
+					<h2>Expertise</h2>
+				</header>
+				<section>
+					<header>
+						<h3>UX Design</h3>
+					</header>
+					<p>Listed below are the design elements of any successful UX project. Some I work with more than others. However, I have a solid understanding of the complete UX design process.</p>
+					<ul>
+						<li>User and Product Goals</li>
+		                <li>Functional and Content Requirements</li>
+		                <li>Information Architecture</li>
+		                <li>Information Design</li>
+		                <li>Interface Design and Navigation Design</li>
+		                <li>Graphic Design</li>
+					</ul>					
+					<aside>
+						<img src="wireframe.png" alt="Wireframe">
+					</aside>
+				</section><?php //end ux design section?>
+				<section>
+					<header>
+						<h3>Web Design</h3>
+					</header>
+					<p> Below are some of the most common tools I use in my UX/Web Design projects.</p>
+                    <ul>
+                        <li>Design tools include Adobe CS suite, Balsamiq, Axure</li>
+                        <li>UI Design Patterns/UI Design Principles</li>
+                        <li>Semantic and standards compliance HTML, CSS, and JS (jQuery, custom widgets)</li>
+                        <li>Grid Systems such as Bootstrap, Blueprint and 960 Grid</li>
+                        <li>Development with PHP, Codeigniter, ASP.NET, XML/XSLT, SASS and Compass</li>
+                        <li>Object-Oriented Development</li>
+                        <li>SEO and web page optimization</li>
+                    </ul>					
+					<aside>
+						<img src="code.png" alt="Code">
+					</aside>
+				</section><?php //end web design section?>
+			</section><?php //end expertise section?>
+			<section class="marketing">
+				<h3>How engaging is your web presence? <strong>Make an impact</strong> Don't be left out in the cold</h3>
+				<img src="images/marketing-message.png" alt="How Engaging Is Your Web Presence? Make An Impact Don't Be Left Out In The Cold">
+			</section><?php //end marketing?>
+			<section class="portfolio">
+				<?php ?>
+			</section>
+			<section class="insipiration">
+				<header>
+					<h2>Insipiration</h2>
+				</header>
+				<section>
+					<h4>People</h4>
+					<ul>
+						<li><a href="http://www.nngroup.com/people/jakob-nielsen/" target="_blank">Jakob Nielsen</a></li>
+	                    <li><a href="http://blog.jjg.net/" target="_blank">Jesse James Garrett</a></li>
+	                    <li><a href="http://www.zeldman.com/" target="_blank">Jeffery Zeldman</a></li>
+	                    <li><a href="http://www.lukew.com/" target="_blank">Luke Wroblewski</a></li>
+					</ul>
+				</section><?php //end People section?>
+				<section>
+				<header>
+					<h4>Websites</h4>
+				</header>
+                <ul class="list">
+                    <li><a href="http://alistapart.com/" target="_blank">A List Apart</a></li>
+                    <li><a href="http://uxmag.com/" target="_blank">UX Magazine</a></li>
+                    <li><a href="http://www.uxmatters.com/index.php" target="_blank">UX Matters</a></li>
+                    <li><a href="http://boxesandarrows.com/" target="_blank">Boxes and Arrows</a></li>
+                    <li><a href="http://ajaxian.com/" target="_blank">Ajaxian</a></li>
+                </ul>
+                </section><?php //end Websites section?>
+			</section><?php //end insipiration section?>
+			<footer>
+				<div id="sharethis">
+					<p>Share this website</p>
+					<span class='st_facebook_large' displayText='Facebook'></span> <span class='st_twitter_large' displayText='Tweet'></span> <span class='st_linkedin_large' displayText='LinkedIn'></span> <span class='st_email_large' displayText='Email'></span>
+				</div>
+				<div class="copyright"> &copy; <?php echo date("Y"); ?> Arctic Web Solutions </div>
+			</footer>
+		</div><?php //end page-content?>
+	</div><?php //end page-wrap?>
+	
+	
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+	
+	<!-- Google Analytics -- use footer include -->
+	<script></script>
+</body>
+</html>
